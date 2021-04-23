9e2bd13e00e71a3a621c2e5eec8da16964152404
Evgeniy Kukushkin <eugin86@list.ru>
2021-04-23 23:34:44 +0500

    Прописал по уроку задания, в коде только поменял название класса истории и директорию её хранения.

--- lesson 2/src/main/java/Client2.java
+++
@@ -1,8 +0,0 @@
-import client.ClientChatAdapter;
-
-public class Client2 {
-    public static void main(String[] args) {
-        new ClientChatAdapter("localhost", 8189);
-    }
-}
-
--- lesson 2/src/main/java/Client3.java
+++
@@ -1,8 +0,0 @@
-import client.ClientChatAdapter;
-
-public class Client3 {
-    public static void main(String[] args) {
-        new ClientChatAdapter("localhost", 8189);
-    }
-}
-
